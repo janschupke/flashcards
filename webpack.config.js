55062d179a1e95189cd1ef8010d9d88f3ae49eac
Jan Schupke <jan@schupke.io>
2025-07-07 23:59:39 +0000

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,52 @@
 const HTMLWebPackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-module.exports = {
-	entry: "./src/index.tsx",
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js',
-		publicPath: '/'
-	},
-	resolve: {
-		extensions: [".js", ".jsx", ".json", ".ts", ".tsx"]
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(ts|tsx)$/,
-				use: {
-					loader: "ts-loader"
-				}
+module.exports = (env, argv) => {
+	const isProduction = argv.mode === 'production';
+	
+	return {
+		mode: isProduction ? 'production' : 'development',
+		entry: "./src/index.tsx",
+		output: {
+			path: path.resolve(__dirname, 'dist'),
+			filename: 'bundle.js',
+			publicPath: '/'
+		},
+		resolve: {
+			extensions: [".js", ".jsx", ".json", ".ts", ".tsx"]
+		},
+		module: {
+			rules: [
+				{
+					test: /\.(ts|tsx)$/,
+					use: {
+						loader: "ts-loader"
+					}
+				},
+				{
+					enforce: "pre",
+					test: /\.js$/,
+					use: {
+						loader: "source-map-loader"
+					}
+				},
+			]
+		},
+		plugins: [
+			new HTMLWebPackPlugin({
+				template: "./src/index.html"
+			})
+		],
+		devServer: {
+			historyApiFallback: true,
+			hot: true,
+			port: 3000,
+			open: true,
+			static: {
+				directory: path.join(__dirname, 'dist'),
 			},
-			{
-				enforce: "pre",
-				test: /\.js$/,
-				use: {
-					loader: "source-map-loader"
-				}
-			},
-		]
-	},
-	plugins: [
-		new HTMLWebPackPlugin({
-			template: "./src/index.html"
-		})
-	]
+			compress: true,
+			liveReload: true
+		}
+	};
 };
